f7af4c61bc9aca0cf1f870539f9e7910bc59a7a5
verze artie <verze.artie@gmail.com>
2024-07-12 18:43:14 +0900

    Update Edelgarzen ( F1 Door-12 )

--- Assets/Script/View/DungeonField.cs
+++ Assets/Script/View/DungeonField.cs
@@ -3759,6 +3759,19 @@ public class DungeonField : MotherBase
           }
           return;
         }
+
+        if (LocationFieldDetect(fieldObjBefore, Fix.EDELGARZEN_1_DOOR_12_X, Fix.EDELGARZEN_1_DOOR_12_Y, Fix.EDELGARZEN_1_DOOR_12_Z))
+        {
+          if (direction == Fix.Direction.Right)
+          {
+            MessagePack.Message1900077(ref QuestMessageList, ref QuestEventList); TapOK();
+          }
+          else
+          {
+            MessagePack.Message1900030(ref QuestMessageList, ref QuestEventList); TapOK();
+          }
+          return;
+        }
       }
       if (One.TF.CurrentDungeonField == Fix.MAPFILE_EDELGARZEN_2)
       {
@@ -10907,6 +10920,10 @@ public class DungeonField : MotherBase
             if (currentMessage == Fix.EDELGARZEN_1_DOOR_11_O)
             {
               RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_11_X, Fix.EDELGARZEN_1_DOOR_11_Y, Fix.EDELGARZEN_1_DOOR_11_Z));
+            }
+            if (currentMessage == Fix.EDELGARZEN_1_DOOR_12_O)
+            {
+              RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_12_X, Fix.EDELGARZEN_1_DOOR_12_Y, Fix.EDELGARZEN_1_DOOR_12_Z));
             }
           }
 
@@ -19981,6 +19998,10 @@ public class DungeonField : MotherBase
       if (One.TF.Event_Message1900076)
       {
         RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_11_X, Fix.EDELGARZEN_1_DOOR_11_Y, Fix.EDELGARZEN_1_DOOR_11_Z));
+      }
+      if (One.TF.Event_Message1900077)
+      {
+        RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_12_X, Fix.EDELGARZEN_1_DOOR_12_Y, Fix.EDELGARZEN_1_DOOR_12_Z));
       }
     }
     if (map_data == Fix.MAPFILE_EDELGARZEN_2)
