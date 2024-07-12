b743865590f74b084a386d0bfefd7a22fe35baa1
verze artie <verze.artie@gmail.com>
2024-07-12 12:25:10 +0900

    Update Edelgarzen ( F1 Door-8 )

--- Assets/Script/Class/TeamFoundation.cs
+++ Assets/Script/Class/TeamFoundation.cs
@@ -2005,6 +2005,7 @@ public class TeamFoundation : MonoBehaviour
   [SerializeField] protected bool _event_message1900070 = false;
   [SerializeField] protected bool _event_message1900071 = false;
   [SerializeField] protected bool _event_message1900072 = false;
+  [SerializeField] protected bool _event_message1900073 = false;
   public bool Event_Message1900010 { get { return _event_message1900010; } set { _event_message1900010 = value; } } // 始めの探索開始イベント
   public bool Event_Message1900020 { get { return _event_message1900020; } set { _event_message1900020 = value; } } // 1F：ワープ鏡１
   public bool Event_Message1900040 { get { return _event_message1900040; } set { _event_message1900040 = value; } } // 1F：扉５
@@ -2031,6 +2032,7 @@ public class TeamFoundation : MonoBehaviour
   public bool Event_Message1900070 { get { return _event_message1900070; } set { _event_message1900070 = value; } } // 2F：扉２０
   public bool Event_Message1900071 { get { return _event_message1900071; } set { _event_message1900071 = value; } } // 1F：扉２
   public bool Event_Message1900072 { get { return _event_message1900072; } set { _event_message1900072 = value; } } // 1F：扉７
+  public bool Event_Message1900073 { get { return _event_message1900073; } set { _event_message1900073 = value; } } // 1F：扉８
 
   #endregion
 
--- Assets/Script/View/DungeonField.cs
+++ Assets/Script/View/DungeonField.cs
@@ -3707,6 +3707,19 @@ public class DungeonField : MotherBase
           }
           return;
         }
+
+        if (LocationFieldDetect(fieldObjBefore, Fix.EDELGARZEN_1_DOOR_8_X, Fix.EDELGARZEN_1_DOOR_8_Y, Fix.EDELGARZEN_1_DOOR_8_Z))
+        {
+          if (direction == Fix.Direction.Bottom)
+          {
+            MessagePack.Message1900073(ref QuestMessageList, ref QuestEventList); TapOK();
+          }
+          else
+          {
+            MessagePack.Message1900030(ref QuestMessageList, ref QuestEventList); TapOK();
+          }
+          return;
+        }
       }
       if (One.TF.CurrentDungeonField == Fix.MAPFILE_EDELGARZEN_2)
       {
@@ -10784,6 +10797,10 @@ public class DungeonField : MotherBase
             if (currentMessage == Fix.EDELGARZEN_1_DOOR_7_O)
             {
               RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_7_X, Fix.EDELGARZEN_1_DOOR_7_Y, Fix.EDELGARZEN_1_DOOR_7_Z));
+            }
+            if (currentMessage == Fix.EDELGARZEN_1_DOOR_8_O)
+            {
+              RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_8_X, Fix.EDELGARZEN_1_DOOR_8_Y, Fix.EDELGARZEN_1_DOOR_8_Z));
             }
           }
 
@@ -19842,6 +19859,10 @@ public class DungeonField : MotherBase
       if (One.TF.Event_Message1900072)
       {
         RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_7_X, Fix.EDELGARZEN_1_DOOR_7_Y, Fix.EDELGARZEN_1_DOOR_7_Z));
+      }
+      if (One.TF.Event_Message1900073)
+      {
+        RemoveFieldObject(FieldObjList, new Vector3(Fix.EDELGARZEN_1_DOOR_8_X, Fix.EDELGARZEN_1_DOOR_8_Y, Fix.EDELGARZEN_1_DOOR_8_Z));
       }
     }
     if (map_data == Fix.MAPFILE_EDELGARZEN_2)
