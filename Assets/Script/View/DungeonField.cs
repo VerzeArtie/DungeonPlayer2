5a2b16c3df6d8e0564c0f272c78db1a3a8b3da4f
verze artie <verze.artie@gmail.com>
2024-07-13 12:35:40 +0900

    Update Edelgarzen ( F2 Mirror-38-R none )

--- Assets/Script/View/DungeonField.cs
+++ Assets/Script/View/DungeonField.cs
@@ -7143,6 +7143,8 @@ public class DungeonField : MotherBase
           {
             MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, (Fix.EDELGARZEN_2_MIRROR_38B_X + 1) + ":1:" + Fix.EDELGARZEN_2_MIRROR_38B_Z); TapOK();
           }
+          // 鏡面ルート
+          // （なし）
           return;
         }
         if (LocationFieldDetect(fieldObjBefore, Fix.EDELGARZEN_2_MIRROR_38B_X, Fix.EDELGARZEN_2_MIRROR_38B_Y, Fix.EDELGARZEN_2_MIRROR_38B_Z))
@@ -7151,6 +7153,8 @@ public class DungeonField : MotherBase
           {
             MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, (Fix.EDELGARZEN_2_MIRROR_38_X - 1) + ":1:" + Fix.EDELGARZEN_2_MIRROR_38_Z); TapOK();
           }
+          // 鏡面ルート
+          // （なし）
           return;
         }
 
