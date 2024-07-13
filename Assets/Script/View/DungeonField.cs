54c87af54aafd0b07d8b4e7fe8453739fefdaae8
verze artie <verze.artie@gmail.com>
2024-07-13 12:22:10 +0900

    Update Edelgarzen ( F2 Mirror-32-R )

--- Assets/Script/View/DungeonField.cs
+++ Assets/Script/View/DungeonField.cs
@@ -6975,6 +6975,11 @@ public class DungeonField : MotherBase
           {
             MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, (Fix.EDELGARZEN_2_MIRROR_32B_X - 1) + ":1:" + Fix.EDELGARZEN_2_MIRROR_32B_Z); TapOK();
           }
+          // 鏡面ルート
+          if (direction == Fix.Direction.Right)
+          {
+            MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, (Fix.EDELGARZEN_2_MIRROR_32B_X + 1) + ":1:" + Fix.EDELGARZEN_2_MIRROR_32B_Z); TapOK();
+          }
           return;
         }
         if (LocationFieldDetect(fieldObjBefore, Fix.EDELGARZEN_2_MIRROR_32B_X, Fix.EDELGARZEN_2_MIRROR_32B_Y, Fix.EDELGARZEN_2_MIRROR_32B_Z))
@@ -6982,6 +6987,11 @@ public class DungeonField : MotherBase
           if (direction == Fix.Direction.Right)
           {
             MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, (Fix.EDELGARZEN_2_MIRROR_32_X + 1) + ":1:" + Fix.EDELGARZEN_2_MIRROR_32_Z); TapOK();
+          }
+          // 鏡面ルート
+          if (direction == Fix.Direction.Left)
+          {
+            MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, (Fix.EDELGARZEN_2_MIRROR_32_X - 1) + ":1:" + Fix.EDELGARZEN_2_MIRROR_32_Z); TapOK();
           }
           return;
         }
