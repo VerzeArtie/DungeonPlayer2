8aff0809f5e963e5d792c331edab87d7f71c80c7
verze artie <verze.artie@gmail.com>
2024-07-26 23:15:00 +0900

    Update Edelgarzen ( F1 Mirror-29 )

--- Assets/Script/View/DungeonField.cs
+++ Assets/Script/View/DungeonField.cs
@@ -6871,6 +6871,17 @@ public class DungeonField : MotherBase
           MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, Fix.EDELGARZEN_1_MIRROR_28_X + ":1:" + Fix.EDELGARZEN_1_MIRROR_28_Z); TapOK();
           return;
         }
+
+        if (LocationFieldDetect(fieldObjBefore, Fix.EDELGARZEN_1_MIRROR_29_X, Fix.EDELGARZEN_1_MIRROR_29_Y, Fix.EDELGARZEN_1_MIRROR_29_Z))
+        {
+          MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, Fix.EDELGARZEN_1_MIRROR_29B_X + ":1:" + Fix.EDELGARZEN_1_MIRROR_29B_Z); TapOK();
+          return;
+        }
+        if (LocationFieldDetect(fieldObjBefore, Fix.EDELGARZEN_1_MIRROR_29B_X, Fix.EDELGARZEN_1_MIRROR_29B_Y, Fix.EDELGARZEN_1_MIRROR_29B_Z))
+        {
+          MessagePack.Message1900021(ref QuestMessageList, ref QuestEventList, Fix.EDELGARZEN_1_MIRROR_29_X + ":1:" + Fix.EDELGARZEN_1_MIRROR_29_Z); TapOK();
+          return;
+        }
       }
       else if (One.TF.CurrentDungeonField == Fix.MAPFILE_EDELGARZEN_2)
       {
