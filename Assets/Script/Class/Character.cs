f5f9daa15bb78123a9bd4d47092cec89b7642605
verze artie <verze.artie@gmail.com>
2024-08-18 18:30:33 +0900

    Tuning Monster

--- Assets/Script/Class/Character.cs
+++ Assets/Script/Class/Character.cs
@@ -7310,7 +7310,7 @@ public partial class Character : MonoBehaviour
       case Fix.DEVIL_STAR_DEATH_FLODIETE:
       case Fix.DEVIL_STAR_DEATH_FLODIETE_JP:
       case Fix.DEVIL_STAR_DEATH_FLODIETE_JP_VIEW:
-        SetupParameter(1500, 650, 2000, 300000, 85, 0, 60000, 70000);
+        SetupParameter(1500, 650, 2000, 30000, 85, 0, 60000, 70000);
         list.Add(Fix.MAGIC_ATTACK);
         list.Add(Fix.BLUE_BULLET);
         list.Add(Fix.COMMAND_AQUA_BLOSSOM);
