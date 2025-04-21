8af4d68341ab33346bd8a1aec8c0b4be0855424b
verze artie <verze.artie@gmail.com>
2025-04-21 21:16:09 +0900

    Update etc

--- Assets/Script/Class/GameConfig.cs
+++ Assets/Script/Class/GameConfig.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GameConfig : MonoBehaviour
 {
-  public int EnableBGM { get; set; }
-  public int EnableSoundEffect { get; set; }
-  public int Difficulty { get; set; }
-  public bool SupportLog { get; set; }
-  public string Account { get; set; }
+  protected int _enableBGM = 0;
+  protected int _enableSound = 0;
+  protected int _difficulty = 0;
+  protected bool _supportLog = true;
+  protected string _account = string.Empty;
+
+  public int EnableBGM { get { return _enableBGM; } set { _enableBGM = value; } }
+  public int EnableSoundEffect { get { return _enableSound; } set { _enableSound = value; } }
+  public int Difficulty { get { return _difficulty; } set { _difficulty = value; } }
+  public bool SupportLog { get { return _supportLog; } set { _supportLog = value; } }
+  public string Account { get { return _account; } set { _account = value; } }
 }
